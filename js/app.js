fa36cb721cd15e279c5ed3cc37a142241125ee7c
Daniele T <Fupete@users.noreply.github.com>
2023-05-16 20:37:11 +0000

    refactor: app.js

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@
 // - ...
 
 import "./init"
-
 import * as THREE from 'three'
 global.THREE = THREE // global THREE
 
@@ -27,36 +26,19 @@ let current_sketch = 0
 let current_set = 0
 let isFullscreen = false
 let showCursor = false
-
-const init = () => {
-	window.document.body.style.cursor = 'none'
-	changeSketch(0)
-	// setTimeout(() => {
-	// 	toggleFullscreen();
-	//   }, 5000);
-	// ...
-}
-window.addEventListener('load', init)
 global.canvas3D = document.getElementById("canvas3D") // global canvas3D
 
 // CHANGE SET & SKETCH
 const changeSet = (set) => {
 	current_set = set
-	// console.log("changeSet: " + current_set)
 	changeSketch(0)
 }
 const changeSketch = (sketch) => {
-	reset()
 	current_sketch = sketch
 	const loc = current_set + '/' + current_sketch
 	const sketchName = artFolder + '/' + loc + '.js'
 	loadSketch(sketchName)
-	// console.log("changeSketch: " + sketchName)
 	document.location.hash = loc
-}
-const reset = () => {
-	console.log('reset if need, init')
-	// ...
 }
 const loadSketch = async (sketchName) => {
 	if (myThree?.dispose()) {
@@ -70,11 +52,10 @@ const loadSketch = async (sketchName) => {
 
 // UI
 const onKeyDown = (event) => {
-	// console.log(event.keyCode)
 	var keyCode = event.keyCode
 	if (keyCode >= 65 && keyCode <= 90)
-		changeSketch(keyCode - 65) // CHANGE SKETCH // keys a-z
-	else if (keyCode >= 48 && keyCode <= 57) { // CHANGE SET // keys 0-9
+		changeSketch(keyCode - 65) // a-z
+	else if (keyCode >= 48 && keyCode <= 57) { // 0-9
 		changeSet(keyCode - 48)
 	} else if (keyCode == 220) toggleMouse() // \
 	else if (keyCode == 117) toggleFullscreen() // F6
@@ -83,13 +64,11 @@ window.addEventListener('keydown', function (e) {
 	if (typeof onKeyDown == 'function') onKeyDown(e);
 })
 const toggleMouse = () => {
-	console.log("toggleMouse")
 	showCursor = !showCursor
 	if (showCursor) window.document.body.style.cursor = 'crosshair'
 	else window.document.body.style.cursor = 'none'
 }
 const toggleFullscreen = () => {
-	console.log("toggleFullscreen")
 	isFullscreen = false
 	if (document.fullscreenElement == null) {
 		canvas3D.requestFullscreen()
@@ -99,3 +78,14 @@ const toggleFullscreen = () => {
 		isFullscreen = false
 	}
 }
+
+// INIT
+const init = () => {
+	window.document.body.style.cursor = 'none'
+	changeSketch(0)
+	// setTimeout(() => {
+	// 	toggleFullscreen();
+	//   }, 5000);
+	// ...
+}
+window.addEventListener('load', init)
