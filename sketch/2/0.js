b9047333289d5c19b44634c8091cc3b43259323d
gretablt <gre.baltieri@gmail.com>
2023-05-22 18:00:38 +0200

    fix: typo

--- sketch/2/0.js
+++ sketch/2/0.js
@@ -1,5 +1,5 @@
 // Rotating cube
-import * as THREE from "three";
+import * as THREE from "three"; // xxx
 //import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Stats from 'three/addons/libs/stats.module.js' // XXX
