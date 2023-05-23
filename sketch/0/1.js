94e5a18b7128586566bd4064cc948c23b2267016
emmlle <m.digennaro@studio.unirsm.sm>
2023-05-23 08:22:05 +0200

    feat: texture 1.js

--- sketch/0/1.js
+++ sketch/0/1.js
@@ -1,67 +1,105 @@
-import * as THREE from 'three';
+
+import Stats from 'three/examples/jsm/libs/stats.module.js' 
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
-import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
-//const hdrTextureURL = new URL('img/studio_small_08_8k.hdr', import.meta.URL);
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-//background color
-renderer.setClearColor(0x00ff00);
-//scena e camera
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-const orbit = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 0, 5);
-orbit.update();
-/*const gridHelper = new THREE.GridHelper(12, 12);
-scene.add(gridHelper);
-// Sets the x, y, and z axes with each having a length of 4
-const axesHelper = new THREE.AxesHelper(4);
-scene.add(axesHelper);*/
-//const loader = new RGBELoader();
-//loader.load(hdrTextureURL, function(texture){
-//texture.mapping = THREE.EquirectangularReflectionMapping;
-    //scene.background = texture;
-    //scene.environment = texture;
-    //sfera lucida
-    const sphere = new THREE.Mesh(
-        new THREE.SphereGeometry(1, 50, 50),
-        new THREE.MeshPhysicalMaterial({
-            roughness: 0,
-            metalness: 1,
-            color: 0xFEFEFE
-           // transmission: 1,
-            //ior: 2.33
+
+
+
+export function sketch(canvas3D, THREE) {
+    console.log("Sketch launched")
+    const stats = new Stats() // XXX
+    canvas3D.appendChild(stats.dom)
+
+
+// RENDERER
+        renderer = new THREE.WebGLRenderer({
+            alpha: true,
+            antialias: true
         })
-    );
-    scene.add(sphere);
-    sphere.position.x = 1.5;
-   //sfera opaca
-   const sphere2 = new THREE.Mesh(
-        new THREE.SphereGeometry(1, 50, 50),
-        new THREE.MeshPhysicalMaterial({
-            roughness: 0.6,
-            metalness: 0,
-            color: 0xFEFEFE
-            //envMap: texture
-        })
-    );
-    scene.add(sphere2);
-    sphere2.position.x = -1.5;
-//})
-//animazione sfera
-function animate() {
-    renderer.render(scene, camera);
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        canvas3D.appendChild(renderer.domElement)
+        renderer.setAnimationLoop(animate);
+     
+
+// CAMERA
+    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000)
+    camera.position.z = 1000
+
+
+// WINDOW RESIZE
+    const onWindowResize = () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+    }
+    window.addEventListener('resize', onWindowResize) 
+    
+// CONTROLS
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+//SCENE
+const scene = new THREE.Scene()
+
+ //sfere
+const sphere = new THREE.SphereGeometry(1, 50, 50)
+const sphere2 = new THREE.SphereGeometry(1, 50, 50)
+const material1 = new THREE.MeshSatandarMaterial({ 
+    map: colorTexture,
+    roughness: .1, 
+    metalness: .9
+})
+const material2 = new THREE.MeshSatandarMaterial({ 
+    color: 0xff0000,
+    roughness: .9, 
+    metalness: .5
+})
+const mesh = new THREE.Mesh(sphere, sphere2, material)
+scene.add(mesh)
+sphere.positin.x = 1.5;
+sphere2.position.x = -1.5;
+//texture
+const textureLoader = new THREE.TextureLoader( loadingManager)
+const colorTexture = textureLoader.load ('public/assets/texture/cube/MilkyWay/dark-s_pz.jpg')
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREENearestFilter
+
+
+// LIGHTS
+    const light = new THREE.DirectionalLight(0xffffff)
+    light.position.set(0.5, 0.5, 1)
+    scene.add(light)
+    const pointLight = new THREE.PointLight(0xff0000)
+    pointLight.position.set(0, 0, 100)
+    scene.add(pointLight)
+    const ambientLight = new THREE.AmbientLight(0xfffa00)
+    scene.add(ambientLight)
+
+
+//animate
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    //update controls
+    controls.update()
+
+    //render
+    renderer.render(scene, camera)
+    stats.end() // XXX
+
+    //chiamo tick
+    window.requestAnimationFrame( tick )
+
 }
-renderer.setAnimationLoop(animate);
-window.addEventListener('resize', function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
+
+tick()
+
+}
+
+export function dispose() {
+    cancelAnimationFrame(animation)
+    renderer.dispose()
+    geometry.dispose()
+    material.dispose()
+    window.removeEventListener('resize', onWindowResize)
+}
