22106fa70731310df1d0a77d2e557ed62e4e7345
Daniele T <Fupete@users.noreply.github.com>
2023-05-27 17:23:18 +0200

    chore: update install base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import dynamicImport from 'vite-plugin-dynamic-import'
 
 export default {
-  // base: '/three-sessions-2023-senigallia/',
   plugins: [
     dynamicImport(/* options */)
   ]
 }
-
-// --base="/three-sessions-2023-senigallia/"
