117e758385b636cf57f723c4e80ab1629be0c366
Daniele T <Fupete@users.noreply.github.com>
2023-05-30 19:49:42 +0200

    feat: 6 slideshow finale

--- sketch/6/1.js
+++ sketch/6/1.js
@@ -1,13 +1,28 @@
+// import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js'
+// import { RenderPass } from 'three/addons/postprocessing/RenderPass.js'
+// import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js'
+
 let scene
-let material
+let material, material2
 let geometry
 let animation
 let onWindowResize
 let slideShow
+// let composer
+// let renderPass
+// let bloomPass
 let images = []
 
 export function sketch() {
     console.log("Sketch launched")
+
+    // const p = {
+    //     // bloom
+    //     exposure: 0.0,
+    //     bloomStrength: 0,
+    //     bloomThreshold: .7,
+    //     bloomRadius: .01,
+    // }
 
     const loadImage = (path, format) => {
         const imageLoader = new THREE.TextureLoader()
@@ -19,19 +34,16 @@ export function sketch() {
         )
     }
 
-    loadImage('https://allyourhtml.club/carousel/lion', '.jpg')
-    loadImage('https://allyourhtml.club/carousel/lion', '.jpg')
-    loadImage('https://allyourhtml.club/carousel/lion', '.jpg')
-    loadImage('https://allyourhtml.club/carousel/lion', '.jpg')
-    loadImage('https://allyourhtml.club/carousel/lion', '.jpg')
+    for (let x = 1; x < 46; x++) {
+        loadImage(`./assets/coda/img_${x}`, '.jpg')
+    }
 
-
-    const imageDefault = images[0]
-    const imageAspect = 1.77
+    const imageDefault = null
+    const imageAspect = 1
 
     // CAMERA
     const camera = new THREE.OrthographicCamera(
-        -0.5, 0.5, 0.5, -0.5, // bounds
+        -1, 1, .5, -1, // bounds
         -10, 10
     )
     camera.position.z = 5
@@ -44,8 +56,10 @@ export function sketch() {
         const viewportAspect = window.innerWidth / window.innerHeight
         if (imageAspect > viewportAspect) {
             shaderScale.set(imageAspect / viewportAspect, 1)
+            shaderScale2.set(imageAspect / viewportAspect, 1)
         } else {
             shaderScale.set(1, viewportAspect / imageAspect)
+            shaderScale2.set(1, viewportAspect / imageAspect)
         }
     }
     window.addEventListener('resize', onWindowResize)
@@ -69,19 +83,39 @@ export function sketch() {
                          varying vec2 vUv;
                          void main(){
                             // SCALE, background size cover
-                            vec2 newUV = (vUv - vec2(0.5))/scale + vec2(0.5);
+                            vec2 newUV = (vUv - vec2(0.5)) / scale + vec2(0.5);
                             gl_FragColor = texture2D(uTexture,newUV);
                          }`
     })
+    material2 = material.clone()
     const shaderScale = material.uniforms.scale.value
+    const shaderScale2 = material2.uniforms.scale.value
     slideShow = setInterval(() => {
         material.uniforms.uTexture.value = images[Math.round(Math.random() * images.length)]
-        console.log('image changed random')
-    }, 2000)
+        // console.log('image changed random')
+    }, 6000)
+    slideShow = setInterval(() => {
+        material2.uniforms.uTexture.value = images[Math.round(Math.random() * images.length)]
+        // console.log('image changed random')
+    }, 3000)
 
     const mesh = new THREE.Mesh(geometry, material)
+    const mesh2 = new THREE.Mesh(geometry, material2)
     scene.add(mesh)
+    mesh.position.x = -.5
+    scene.add(mesh2)
+    mesh2.position.x += .5
     onWindowResize()
+
+    // // POST-PROCESSING
+    // composer = new EffectComposer(renderer)
+    // renderPass = new RenderPass(scene, camera)
+    // composer.addPass(renderPass)
+    // bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+    // bloomPass.threshold = p.bloomThreshold
+    // bloomPass.strength = p.bloomStrength
+    // bloomPass.radius = p.bloomRadius
+    // composer.addPass(bloomPass)
 
     // ANIMATE
     const animate = () => {
@@ -89,8 +123,11 @@ export function sketch() {
 
         // ANIMATION
         // ...
+        // ...
+        // bloomPass.strength = MIC.getHighsVol(1.5, 5)
 
         renderer.render(scene, camera) // RENDER
+        // composer.render() // POST-PROCESSING
         if (showStats) stats.end() // XXX
 
         animation = requestAnimationFrame(animate) // CIAK
@@ -103,9 +140,12 @@ export function dispose() {
     if (slideShow) clearInterval(slideShow)
     geometry?.dispose()
     material?.dispose()
-    for (let i = 0; i < images.length; i++) {
-        images[i].dispose()
+    material2?.dispose()
+    if (images?.length) {
+        for (let i = 0; i < images.length; i++) {
+            images[i].dispose()
+        }
+        images = []
     }
-    images = null
     window.removeEventListener('resize', onWindowResize)
 }
